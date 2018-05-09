3ecffff15afe106c914e254dde949db60e0c2821
Louis-marie JOLY <ljoly@e2r11p1.42.fr>
2018-05-09 12:52:22 +0200

    std of stds

--- histogram.py
+++ histogram.py
@@ -1,5 +1,7 @@
 import csv
 import sys
+import math
+import numpy as np
 import matplotlib.pyplot as plt
 
 csvfile = open('assets/dataset_train.csv')
@@ -13,13 +15,28 @@ for i in range(len(rawdata[0]) - indexFeatures):
     features.append(rawdata[0][i + indexFeatures])
 
 # Arrays of marks for each house
-gryf = [[] * lenFeatures]
-raven = [[] * lenFeatures]
-slyth = [[] * lenFeatures]
-huffle = [[] * lenFeatures]
+gryf = [[], [], [], [], [], [], [], [], [], [], [], [], []]
+raven = [[], [], [], [], [], [], [], [], [], [], [], [], []]
+slyth = [[], [], [], [], [], [], [], [], [], [], [], [], []]
+huffle = [[], [], [], [], [], [], [], [], [], [], [], [], []]
 
-# Array of houses
+# Array of arrays of marks
 houses = [gryf, raven, slyth, huffle]
+
+# Arrays of Std Deviations
+gryfStd = [0.0] * lenFeatures
+ravenStd = [0.0] * lenFeatures
+slythStd = [0.0] * lenFeatures
+huffleStd = [0.0] * lenFeatures
+
+# Array of arrays of Std arrays
+housesStd = [gryfStd, ravenStd, slythStd, huffleStd]
+
+# Array of Std of Std
+featuresStd = [0.0] * lenFeatures
+
+# Array of Mean of Std
+stdMean = [0.0] * lenFeatures
 
 
 def normalizeData(topic, minV, maxV):
@@ -28,13 +45,16 @@ def normalizeData(topic, minV, maxV):
     return topic
 
 
-def prepareData(topic):
-    for i, elem in enumerate(topic):
-        # Replace empty strings by '0' value
-        if not elem:
-            topic[i] = 0.0
-        else:
-            topic[i] = float(elem)
+def getStd(topic, mean):
+    std = 0
+    for mark in topic:
+        std += (mark - mean) * (mark - mean)
+    std /= len(topic)
+    std = math.sqrt(std)
+    return std
+
+
+def getMinMax(topic):
     # Get min and max values
     topic = sorted(topic)
     minV = topic[0]
@@ -42,33 +62,81 @@ def prepareData(topic):
     return minV, maxV
 
 
+def getMean(topic):
+    mean = 0
+    for i, elem in enumerate(topic):
+        # Replace empty strings by '0' value
+        if not elem:
+            topic[i] = 0.0
+        else:
+            topic[i] = float(elem)
+        mean += topic[i]
+    mean /= len(topic)
+    return mean
+
+
 del rawdata[0]
 for row in rawdata:
-    tmp = [[] * lenFeatures]
-    if row[1] == 'Gryffindor':
-        tmp = gryf
-    elif row[1] == 'Ravenclaw':
+    tmp = gryf
+    if row[1] == 'Ravenclaw':
         tmp = raven
     elif row[1] == 'Slytherin':
         tmp = slyth
     elif row[1] == 'Hufflepuff':
         tmp = huffle
-    for j, _ in enumerate(tmp):
+    for j in range(len(tmp)):
         tmp[j].append(row[j + indexFeatures])
 
-for house in houses:
-    for row in house:
-        minV, maxV = prepareData(row)
+# Get all stats
+for i, house in enumerate(houses):
+    for j, row in enumerate(house):
+        mean = getMean(row)
+        stdMean[j] += mean
+        minV, maxV = getMinMax(row)
+        housesStd[i][j] = getStd(row, mean)
         row = normalizeData(row, minV, maxV)
 
-# plt.title('Houses')
-plt.hist(gryf)
-plt.hist(raven)
-plt.hist(slyth)
-plt.hist(huffle)
+for i, mean in enumerate(stdMean):
+    stdMean[i] /= lenFeatures
+
+print(stdMean)
+
+# Get Std
+for i, house in enumerate(housesStd):
+    for j, row in enumerate(house):
+        featuresStd[j] += (row - stdMean[j]) * (row - stdMean[j])
+
+for i, std in enumerate(featuresStd):
+    featuresStd[i] /= len(houses)
+    featuresStd[i] = math.sqrt(featuresStd[i])
+
+
+print(featuresStd)
+
+kwargs = dict(histtype='stepfilled', alpha=0.3)
+
+plt.figure()
+plt.hist(gryf[0], **kwargs)
+plt.hist(raven[0], **kwargs)
+plt.hist(slyth[0], **kwargs)
+plt.hist(huffle[0], **kwargs)
+plt.title('Arithmancy')
 plt.legend(['Gryffindor', 'Ravenclaw', 'Slytherin', 'Hufflepuff'])
 plt.xlabel('Marks', fontsize=16)
 plt.ylabel('Students', fontsize=16)
 plt.xticks(fontsize=14)
 plt.yticks(fontsize=14)
+
+plt.figure()
+plt.hist(gryf[8], **kwargs)
+plt.hist(raven[8], **kwargs)
+plt.hist(slyth[8], **kwargs)
+plt.hist(huffle[8], **kwargs)
+plt.title('Potions')
+plt.legend(['Gryffindor', 'Ravenclaw', 'Slytherin', 'Hufflepuff'])
+plt.xlabel('Marks', fontsize=16)
+plt.ylabel('Students', fontsize=16)
+plt.xticks(fontsize=14)
+plt.yticks(fontsize=14)
+
 plt.show()
